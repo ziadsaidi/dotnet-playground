7533a5b789279b67c677df381ac89650141dddb0
saidi zied <zied.saidi@inov-tech.fr>
2025-04-10 21:13:20 +0200

    cleaning  :>

--- Sales.Application/Common/Interfaces/IAuthService.cs
+++ Sales.Application/Common/Interfaces/IAuthService.cs
@@ -7,5 +7,5 @@ public interface IAuthService
   string GenerateToken(Guid userId, string userEmail);
   Task<bool> ValidateUserAsync(string email, string password, CancellationToken cancellationToken);
 
-  public ClaimsPrincipal? GetPrincipalFromToken(string token);
+  ClaimsPrincipal? GetPrincipalFromToken(string token);
 }
